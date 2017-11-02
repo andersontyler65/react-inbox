5ce663dab27d45898c1b900c8893c86c532fab56
andersontyler65 <andersontyler65@gmail.com>
2017-11-02 14:48:39 -0600

    Starting Inbox

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Toolbar from './components/Toolbar'
+import Messages from './components/Messages'
 
 class App extends Component {
+ constructor(props) {
+   super(props)
+   this.state = { messages: props.messages}
+ }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Toolbar />
+        <Messages messages={ this.state.messages } />
       </div>
-    );
+    )
   }
 }
 
